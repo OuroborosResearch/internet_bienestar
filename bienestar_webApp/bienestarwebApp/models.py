b9ea6bf0e887a4e47edcc23431d7a65b759109fd
Osman Esquivel Gonzalez <osmanes86@yahoo.com.mx>
2023-10-06 18:23:12 -0600

    Change to Internet Bienestar

---
+++ bienestar_webApp/bienestarwebApp/models.py
@@ -0,0 +1,178 @@
+from django.db import models
+from ckeditor.fields import RichTextField
+from django.conf import settings
+
+
+# Create your models here.
+class Paginas(models.Model):
+    titulo = models.CharField(
+        verbose_name="Título",
+        max_length=200)
+    contenido = RichTextField(
+        verbose_name="Contenido",blank=True, null=True)
+    imagen = models.ImageField(
+        upload_to="pags/bienestarwebApp/terminos", 
+        blank=True, null=True, 
+    )
+    pdf = models.FileField(
+        upload_to="pags/bienestarwebApp/pdfs",
+        null=True,
+    )
+
+    created = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    updated = models.DateTimeField(auto_now=True, blank=True, null=True)
+
+    class Meta:
+        verbose_name = 'Páginas'
+        verbose_name_plural = 'Páginas'
+        # Ordenar de más nuevos a más viejos
+        
+    def __str__(self):
+        return self.titulo
+
+class Planes(models.Model):
+    titulo = models.CharField(
+        verbose_name="Título",
+        max_length=200)
+    contenido = models.TextField(
+        verbose_name="Contenido",blank=True, null=True)
+    imagen = models.ImageField(
+        upload_to="bienestarwebApp/planes", 
+        blank=True, null=True, 
+    )
+
+    created = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    updated = models.DateTimeField(auto_now=True, blank=True, null=True)
+
+    class Meta:
+        verbose_name = 'Planes'
+        verbose_name_plural = 'Planes'
+        # Ordenar de más nuevos a más viejos
+        
+    def __str__(self):
+        return self.titulo
+    
+class Reviews(models.Model):
+    titulo = models.CharField(
+        verbose_name="Título",
+        max_length=200)
+    contenido = models.TextField(
+        verbose_name="Contenido",blank=True, null=True)
+    imagen = models.ImageField(
+        upload_to="terminos", 
+        blank=True, null=True, 
+    )
+
+    created = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    updated = models.DateTimeField(auto_now=True, blank=True, null=True)
+
+    class Meta:
+        verbose_name = 'Reviews'
+        verbose_name_plural = 'Reviews'
+        # Ordenar de más nuevos a más viejos
+        
+    def __str__(self):
+        return self.titulo
+    
+class Blog(models.Model):
+
+
+    titulo = models.CharField(
+        verbose_name="Título",
+        max_length=200)
+    contenido = models.TextField(
+        verbose_name="Contenido",blank=True, null=True)
+    imagen = models.ImageField(
+        upload_to="terminos", 
+        blank=True, null=True, 
+    )
+
+    created = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    updated = models.DateTimeField(auto_now=True, blank=True, null=True)
+
+    class Meta:
+        verbose_name = 'Blog'
+        verbose_name_plural = 'Blog'
+        # Ordenar de más nuevos a más viejos
+        
+    def __str__(self):
+        return self.titulo
+    
+
+class Faqs (models.Model):
+    pregunta = models.CharField(
+        verbose_name="Pregunta",
+        max_length=250)
+    respuesta = RichTextField(
+        verbose_name="Respuesta",blank=True, null=True)
+
+    created = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    updated = models.DateTimeField(auto_now=True, blank=True, null=True)
+
+    class Meta:
+        verbose_name = 'FAQS'
+        verbose_name_plural = 'FAQS'
+        # Ordenar de más nuevos a más viejos
+        
+    def __str__(self):
+        return str(self.pk)
+    
+
+class Aviso (models.Model):
+    titulo = models.CharField(
+        verbose_name="Título",
+        max_length=250)
+    contenido = RichTextField(
+        verbose_name="Contenido",blank=True, null=True)
+
+    created = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    updated = models.DateTimeField(auto_now=True, blank=True, null=True)
+
+    class Meta:
+        verbose_name = 'Aviso de Privacidad'
+        verbose_name_plural = 'Aviso de Privacidad'
+        # Ordenar de más nuevos a más viejos
+        
+    def __str__(self):
+        return str(self.pk)
+    
+    
+
+class Entradas(models.Model):
+    titulo = models.CharField(max_length=100, verbose_name="Título")
+    autor = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,
+                              limit_choices_to={'is_staff': True})
+    etiquetas = models.ManyToManyField('Etiquetas')
+    imagen = models.ImageField(
+        upload_to="Imagen de portada", 
+        blank=True, null=True, 
+    )
+    contenido = RichTextField(
+        verbose_name="Contenido",blank=True, null=True)
+
+    created = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    updated = models.DateTimeField(auto_now=True, blank=True, null=True)
+
+    class Meta:
+        verbose_name = 'Entradas Blog'
+        verbose_name_plural = 'Entradas Blog'
+        # Ordenar de más nuevos a más viejos
+        
+    def __str__(self):
+        return self.titulo
+    
+
+class Etiquetas(models.Model):
+    name = models.CharField(max_length=50)
+
+    created = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    updated = models.DateTimeField(auto_now=True, blank=True, null=True)
+
+    class Meta:
+        verbose_name = 'Etiquetas Blog'
+        verbose_name_plural = 'Etiquetas Blog'
+        # Ordenar de más nuevos a más viejos
+        
+    def __str__(self):
+        return str(self.name)
+
---
+++ bienestar_webApp/bienestarwebApp/views.py
@@ -0,0 +1,281 @@
+from django.shortcuts import render, get_object_or_404, redirect
+import mercadopago
+from django.template import RequestContext
+from django.views.decorators.csrf import csrf_protect
+import datetime
+from .models import Paginas, Faqs, Entradas, Reviews, Planes, Aviso
+from web_api.api import get_offerings, check_cobertura, get_preferences, get_profile_data_clean, get_all_mb_plans, get_all_mifi_plans, get_offer_id, get_offer_price, get_users_length, getUserByNumber, send_subscriptions
+from django.http import HttpResponseRedirect
+from django.conf import settings
+import json
+import requests
+
+
+# SDK de Mercado Pago
+import mercadopago
+# Agrega credenciales
+sdk = mercadopago.SDK("PROD_ACCESS_TOKEN")
+
+# Create your views here.
+
+
+def intro(request):
+
+    if request.method == 'POST':
+        print("[Info][Landing.Intro] Entring the post...")
+
+        name = ""
+        number = ""
+        email = ""
+
+        if request.POST.get('name'):
+            name = request.POST.get('name')
+        if request.POST.get('cel'):
+            number = request.POST.get('cel')
+        if request.POST.get('sub_email'):
+            email = request.POST.get('sub_email')
+
+        # Guardar en BD
+        prospect_dict = {
+            "name" : name,
+            "number" : number,
+            "email" : email
+        }
+
+        send_subscriptions(prospect_dict)
+
+
+    # call traductor
+    planesMB = get_offerings("MB")
+    planesMB = json.loads(planesMB)
+
+    # cantidad de usuarios
+    reviews_ = Reviews.objects.all()
+
+    # Info para app usuarios
+    usersLen = get_users_length()
+    print(type(usersLen))
+    print(int(usersLen))
+    user_Len = int(usersLen)
+    user_Len = usersLen / 10 
+
+    usr_descargas = 25
+    usr_clientesSatisfechos = 100
+    usr_estados = 15
+
+    Faqs_ = Faqs.objects.all()[:5]
+    Entradas_ = Entradas.objects.all()
+    
+    print(user_Len)
+
+    return render(request, 'bienestarwebApp/index.html',
+                  {
+                      "planes": planesMB,
+                      'usersLen': usersLen,
+                      'usr_descargas' : usr_descargas,
+                      'usr_clientesSatisfechos' : usr_clientesSatisfechos,
+                      'usr_estados' : usr_estados,
+                      'user_Len' : int(user_Len),
+                      "Faqs": Faqs_,
+                      "Entradas" : Entradas_,
+                      "reviews" : reviews_,
+                  })
+
+
+def avisos_privacidad(request):
+
+    avisos_ = Aviso.objects.all()
+    return render(request, "bienestarwebApp/aviso/aviso.html", {"avisos": avisos_ })
+
+def cobertura(request):
+    #return render(request, "bienestarwebApp/HBB/cobertura.html")
+    return render(request, "bienestarwebApp/paginas/cobertura.html")
+
+def paginas(request, page_name):
+    pags = get_object_or_404(Paginas, titulo=page_name)
+
+    privacidad_content = Paginas.objects.exclude(titulo__contains="_")
+
+    context = {
+        "pags": pags
+    }
+
+    if 'aviso_privacidad' in page_name:
+        context["privacidad"] = privacidad_content
+
+    return render(request, "bienestarwebApp/paginas/paginas.html", context)
+
+def faqs(request):
+    Faqs_ = Faqs.objects.all()
+    context = {
+        "Faqs": Faqs_
+    }
+
+    return render(request, "bienestarwebApp/faqs/faqs.html", context)
+
+
+def distribuidores(request):
+
+    if settings.DEBUG:
+        host = "127.0.0.1:8000"
+    else:
+        host = "jrmovil.pythonanywhere.com"
+
+    return render(request, "bienestarwebApp/distribuidores/distribuidoresForm.html")
+
+# Just demo
+@csrf_protect
+def mercado_pago_api(request):
+
+    if settings.DEBUG:
+        host = "127.0.0.1:8000"
+    else:
+        host = "jrmovil.pythonanywhere.com"
+
+    if request.method == 'POST':
+
+        print("[Info] Web Mercado pago Init...  ")
+        paquete = None
+
+        if "empty" not in request.POST.get('dropdownMB'):
+            paquete = request.POST.get('dropdownMB')
+        if "empty" not in request.POST.get('dropdownMIFI'):
+            paquete = request.POST.get('dropdownMIFI')
+
+        cel = request.POST.get('idSubscriber')
+        # sub_email = request.POST.get('email')
+
+        # print(request.POST)
+        if paquete:
+            print("[Info][mercado_pago_api] Preparing data for recharge.")
+
+            planId = get_offer_id(paquete)
+            cliente = getUserByNumber(cel)
+            email = "anonymous@gmail.com"
+
+            if(cliente): email = cliente.user_number 
+
+            
+            price = get_offer_price(planId)
+            # Get Preference ID
+            raw = {
+                "title": paquete,
+                "unit_price": price,
+                "idSubscriber": cel,
+                "email": email
+            }
+
+            preference_mo = get_preferences(raw)
+            print(' ***** payment response ****')
+            print(preference_mo)
+
+            #return HttpResponseRedirect(preference_mo["init_point"])
+            return render(request, 'bienestarwebApp/recargas/mp_iframe.html', {"iframe_url" : preference_mo["init_point"]})
+        
+        # Search for JRmóvil
+        uf_response = get_profile_data_clean(cel)
+        if uf_response:
+            return HttpResponseRedirect("http://" + host + "/recargas?" + cel + "ok")
+        else:
+            return HttpResponseRedirect("http://" + host + "/recargas?" + cel + "denied")
+
+    
+    allMB = get_all_mb_plans()
+    allMifi = get_all_mifi_plans()
+    
+
+    return render(request, 'bienestarwebApp/recargas/recargas.html', {
+        "allMB" : allMB,
+        "allMifi" : allMifi
+        })
+
+
+def consultas(request):
+    context = {}
+
+    if request.method == 'POST':
+
+        print("[Info] Web Consulta saldo Init...  ")
+        cel = request.POST.get('idSubscriber')
+        # sub_email = request.POST.get('email')
+
+        print("[Info][mercado_pago_api] Searching client..")  
+        # Search for JRmóvil
+        uf_response = get_profile_data_clean(cel)
+        print("******************** uf_response ")
+        #data = json.load(uf_response)
+        print(uf_response["offeringId"])
+        context["number"] = cel
+        context["paquete_name"] = uf_response["offeringName"]
+        context["expireDate"] = uf_response["expireDate"]
+        context["effectiveDate"] = uf_response["effectiveDate"]
+        context["initialDataAmt"] = uf_response["initialDataAmt"]
+        context["unusedDataAmt"] = uf_response["unusedDataAmt"]
+        context["totalMin"] = uf_response["totalMin"]
+        context["totalUnusedMin"] = uf_response["totalUnusedMin"]
+        context["totalSMS"] = uf_response["totalSMS"]
+        context["totalUnusedSMS"] = uf_response["totalUnusedSMS"]
+      
+
+    return render(request, 'bienestarwebApp/consultas/index.html', context)
+
+def contact(request):
+    return render(request, "bienestarwebApp/contact/contact.html")
+
+def about(request):
+    return render(request, "bienestarwebApp/about/about.html")
+
+def maps_view(request):       
+    return render(request, 'googleMap.html', {'google_maps_api_key': settings.GOOGLE_MAPS_API_KEY})
+
+def autocomplete(request):       
+    return render(request, 'googleMap.html', {'google_maps_api_key': settings.GOOGLE_MAPS_API_KEY})
+
+def hbb(request):
+    responseHbb = None
+    cobertura = False
+    msgResponse = None
+    planes = get_offerings("HBB")
+    planes = json.loads(planes)
+
+
+    if request.POST.get:
+        lat = request.POST.get('latitud')
+        lng = request.POST.get('longitud')
+        direccion = request.POST.get('autocomplete')
+
+        if lat and lng:
+            responseHbb = check_cobertura(lat, lng)
+
+            if "Without Coverage" in responseHbb:
+                responseHbb = "Aún estamos trabajando duro para llevar la cobertura hasta donde estás"   
+            else:
+                responseHbb = "¡Ya contamos con cobertura en donde tú estás!"
+                cobertura = True
+
+            msgResponse = "HBBServiceOn"
+
+            # Limpiar los datos de request.POST
+            request.POST = request.POST.dict()
+            if cobertura:
+                return redirect("hbb_result_ok")
+            else:
+                return redirect("hbb_result_fail")
+
+
+    return render(request, "bienestarwebApp/HBB.html", {
+        'google_maps_api_key': settings.GOOGLE_MAPS_API_KEY, 
+        'responseHbb' : responseHbb,
+        'cobertura':cobertura,
+        'msgResponse' : msgResponse,
+        'planes' : planes
+    })
+
+def hbb_result_fail(request):       
+    return render(request, "bienestarwebApp/HBB/resultFail.html")
+
+def hbb_result_ok(request):       
+    return render(request, "bienestarwebApp/HBB/resultOk.html")
+
+def redireccion_recarga(request):
+    return redirect('https://jrmovil.pythonanywhere.com/recargas')
